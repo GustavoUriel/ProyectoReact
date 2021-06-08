2ec8b0cac4b17fcdf344d04eb5b1235abe5f589d
Gustavo Uriel Levcovich <tygus@outlook.com>
2021-06-07 21:34:07 -0300

    segundoCommit

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+/* import '../styles/NavBar.css'; 
+ */
+class NavBar extends Component {
+  render() {
+    return (
+      <nav>
+        <a className="active" href="#home">PetShop</a>
+        <a href="#news">Productos</a>
+        <a href="#contact">Promociones</a>
+        <a href="#about">About</a>
+    </nav>
+    );
+  }
+}
+
+export default NavBar;
