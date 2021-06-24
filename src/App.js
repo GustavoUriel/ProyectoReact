92f349c05c93657ffa8fdb9ad60d03c179e2001d
Gustavo Uriel Levcovich <tygus@outlook.com>
2021-06-24 11:19:54 -0300

    desafio maps

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import logo from './logo.svg';
+import logo from './img/logo.svg';
 import './App.css';
-import NavBar from './components/NavBar'
-import CartWidget from './components/CartWidget'
-
-
-
+import Listado from './components/listado.js'
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <CartWidget />
-
-{/*       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      https://github.com/GustavoUriel/JSCoderHouse.git
- */}    
+      esta es la app
+      <Listado />
+      volvio a la app
     </div>
   );
 }
---
+++ src/components/listado.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Item from './item.js'
+
+function listado() {
+    return (
+        <div>
+            <Item />
+        </div>
+    );
+}
+
+export default listado
